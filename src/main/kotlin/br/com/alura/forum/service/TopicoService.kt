3eb7dcf84d72a472dd835d41c7cf8ad0b91ea99e
Giovanni <80859834+Giovanni-SVC@users.noreply.github.com>
2022-06-09 19:56:15 -0300

    injecao de dependencia da camada service na controller

---
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -0,0 +1,30 @@
+package br.com.alura.forum.service
+
+import br.com.alura.forum.model.Curso
+import br.com.alura.forum.model.Topico
+import br.com.alura.forum.model.Usuario
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class TopicoService {
+    fun listar(): List<Topico> {
+        val topico = Topico(
+            id = 1,
+            titulo = "Duvida Kotlin",
+            mensagem = "Variaveis no Kotlin",
+            curso = Curso(
+                1,
+                "Kotlin",
+                "Programacao"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Giovanni Cerqueira",
+                email = "giovanni@email.com"
+            )
+        )
+
+        return Arrays.asList(topico, topico, topico)
+    }
+}
