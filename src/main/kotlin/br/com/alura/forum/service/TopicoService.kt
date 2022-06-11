3a3dcbb66b5b8356454ac0f9fa602599e15659e8
Giovanni <80859834+Giovanni-SVC@users.noreply.github.com>
2022-06-11 15:38:28 -0300

    Implementando put

--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -1,12 +1,12 @@
 package br.com.alura.forum.service
 
+import br.com.alura.forum.dto.AtualizacaoTopicoForm
 import br.com.alura.forum.dto.NovoTopicoForm
 import br.com.alura.forum.dto.TopicoView
 import br.com.alura.forum.mapper.TopicoFormMapper
 import br.com.alura.forum.mapper.TopicoViewMapper
 import br.com.alura.forum.model.Topico
 import org.springframework.stereotype.Service
-import java.time.LocalDateTime
 import java.util.stream.Collectors
 import kotlin.collections.ArrayList
 
@@ -18,15 +18,14 @@ class TopicoService(
 ) {
 
     fun listar(): List<TopicoView> {
-        return topicos.stream().map {
-                topico ->
-                topicoViewMapper.map(topico)
+        return topicos.stream().map { topico ->
+            topicoViewMapper.map(topico)
         }.collect(Collectors.toList())
     }
 
     fun buscarPorId(id: Long): TopicoView {
-        val topico = topicos.stream().filter({
-            t -> t.id == id
+        val topico = topicos.stream().filter({ t ->
+            t.id == id
         }).findFirst().get()
 
         return topicoViewMapper.map(topico)
@@ -36,5 +35,21 @@ class TopicoService(
         val topico = topicoFormMapper.map(form)
         topico.id = topicos.size.toLong() + 1
         topicos = topicos.plus(topico)
+    }
+
+    fun atualizar(form: AtualizacaoTopicoForm) {
+        val topico = topicos.stream().filter({ t ->
+            t.id == form.id
+        }).findFirst().get()
+       topicos =  topicos.minus(topico).plus(Topico(
+            id = topico.id,
+            titulo = form.titulo,
+            mensagem = form.mensagem,
+            dataCriacao = topico.dataCriacao,
+            curso = topico.curso,
+            autor = topico.autor,
+            status = topico.status,
+            respostas = topico.respostas
+        ))
     }
 }
