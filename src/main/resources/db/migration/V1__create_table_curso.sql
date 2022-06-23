456f8c76c02e858ea0fe529184b31898aa32c99f
Giovanni <80859834+Giovanni-SVC@users.noreply.github.com>
2022-06-23 20:01:54 -0300

    Implementando migration com flyway

---
+++ src/main/resources/db/migration/V1__create_table_curso.sql
@@ -0,0 +1,6 @@
+create table curso(
+    id bigint not null,
+    nome varchar (50),
+    categoria varchar (50),
+    primary key (id)
+);
---
+++ src/main/resources/db/migration/V2__insert_curso.sql
@@ -0,0 +1,1 @@
+insert into curso(id, nome, categoria) values (1, 'Kotlin', 'Programação');
