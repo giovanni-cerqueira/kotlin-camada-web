ee37d7911464ebf728e0a4f21d75f5501d7a18b1
Giovanni <80859834+Giovanni-SVC@users.noreply.github.com>
2022-07-12 23:55:57 -0300

    Adicionando dependencia do spring security

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.boot:spring-boot-starter-cache")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+
 
 }
 
