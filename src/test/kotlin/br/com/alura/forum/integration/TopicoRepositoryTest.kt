7140f2512cda81d064403dfb3677b46ff8879043
Giovanni <80859834+Giovanni-SVC@users.noreply.github.com>
2022-07-23 22:12:56 -0300

    Testes de integracao

--- src/test/kotlin/br/com/alura/forum/integration/TopicoRepositoryTest.kt
+++ src/test/kotlin/br/com/alura/forum/integration/TopicoRepositoryTest.kt
@@ -1,9 +1,14 @@
 package br.com.alura.forum.integration
 
+import br.com.alura.forum.dto.TopicoPorCategoriaDto
+import br.com.alura.forum.model.TopicoTest
 import br.com.alura.forum.repository.TopicoRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.data.domain.PageRequest
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.MySQLContainer
@@ -17,6 +22,8 @@ class TopicoRepositoryTest {
 
     @Autowired
     private lateinit var topicoRepository: TopicoRepository
+
+    private val topico = TopicoTest.build()
 
     companion object{
         @Container
@@ -35,5 +42,22 @@ class TopicoRepositoryTest {
         }
     }
 
+    @Test
+    fun `deve gerar um relatorio`(){
+        topicoRepository.save(topico)
+        val relatorio = topicoRepository.relatorio()
+
+        assertThat(relatorio).isNotNull
+        assertThat(relatorio.first()).isExactlyInstanceOf(TopicoPorCategoriaDto::class.java)
+    }
+
+    @Test
+    fun `deve listar topico pelo nome do curso`(){
+        topicoRepository.save(topico)
+        topicoRepository.findByCursoNome(topico.curso.nome, PageRequest.of(0, 5))
+
+        assertThat(topico).isNotNull
+
+    }
 
 }
